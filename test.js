0cb77a270496a55b216c2557583dd3b080e2a0dd
shine <shine@goldmorning.cn>
2019-09-30 09:36:30 +0800

    add filename option

--- test.js
+++ test.js
@@ -34,4 +34,5 @@ downloader.download({
     url: 'https://yun.kubozy-youku-163.com/20190709/16666_5a9c65b6/1000k/hls/index.m3u8',
     processNum: 15,
     filePath: 'video'
+    // fileName:'test'
 });
